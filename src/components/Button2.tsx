008252f8a20e8b80ad7770d165d3075bd0b4e523
Akwyn S <akwyns.@MacBook-Pro.local>
2021-04-30 21:46:25 +0700

    ,

--- src/components/Button2.tsx
+++
@@ -1,7 +0,0 @@
-import { Button, DatePicker, Typography, version } from 'antd';
-
-const Button1 = ({ title }: any) => {
-  return <Button type="primary">{title}</Button>;
-};
-
-export default Button1;
